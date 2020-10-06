17babff33a737e8c2d64dc1d09abc8e1d8e2765f
Alexander Gaevsky <sasha@sasha0.ru>
2020-10-06 10:45:53 +0300

    Outline Oscar 2.0 application config.

---
+++ oscar_elasticsearch/apps.py
@@ -0,0 +1,14 @@
+from django.utils.translation import ugettext_lazy as _
+
+from oscar.core.application import OscarConfig
+
+
+class OscarElasticSearchConfig(OscarConfig):
+    label = 'oscar_elasticsearch'
+    name = 'oscar_elasticsearch'
+    verbose_name = _('Elasticsearch')
+
+    def ready(self):
+        from .signal_handlers import register_signal_handlers
+
+        register_signal_handlers()
