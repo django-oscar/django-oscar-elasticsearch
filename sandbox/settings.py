90558c3a5af9ea80c58215a41160e6159f3f49e2
Lars van de Kerkhof <lars@permanentmarkers.nl>
2024-05-10 15:48:59 +0200

    added staticfiles

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-
+    'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
 
